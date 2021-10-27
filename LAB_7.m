9d062a17e84f7d80be947dc881678f6a132510cd
SmithTyler <smithtylerbenjamin@gmail.com>
2021-10-26 23:06:07 -0500

    hw_v1

---
+++ LAB_7.m
@@ -0,0 +1,22 @@
+% Part A 
+clear;
+clc;
+close all;
+
+% Inital variables 
+wp_a = 0.2 * pi;
+ws_a = 0.3 * pi;
+rp_a = 1;
+as_a = 15;
+
+%Function 
+pass_a = 2 * tan(wp_a / 2);
+stop_a = 2 * tan(ws_a / 2);
+[b_a , a_a] = 
+
+
+% Plotting
+t_a = tiledlayout(2,1);
+title(t_a,'Part A Filter')
+
+nexttile
