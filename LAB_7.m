59474eb11d9f2f1e5803e5be1825dfdd59097281
SmithTyler <smithtylerbenjamin@gmail.com>
2021-10-28 13:50:21 -0500

    10/28/2021_V1

--- LAB_7.m
+++ LAB_7.m
@@ -3,16 +3,17 @@ clear;
 clc;
 close all;
 
-% Inital variables 
+% Filter Specs
 wp_a = 0.2 * pi;
 ws_a = 0.3 * pi;
 rp_a = 1;
 as_a = 15;
 
 %Function 
-pass_a = 2 * tan(wp_a / 2);
-stop_a = 2 * tan(ws_a / 2);
-[b_a , a_a] = 
+T_a = 1;
+omgP_a = 2 * tan(wp_a / 2);
+omgS_a = 2 * tan(ws_a / 2);
+
 
 
 % Plotting
